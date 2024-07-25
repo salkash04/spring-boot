48872b72d90e8d038ab09f8ec032550c59595d9f
Салават <salkash04@mail.ru>
2024-07-25 18:12:41 +0300

    initial

---
+++ src/main/java/application/dao/UserDAOImpl.java
@@ -0,0 +1,46 @@
+package application.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import application.model.User;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class UserDAOImpl implements UserDAO {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    @Transactional
+    public void addUser(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    @Transactional
+    public List<User> getUsers(){
+        return entityManager.createQuery("FROM User", User.class).getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void updateUser(User user) {
+        entityManager.merge(user);
+    }
+
+    @Override
+    @Transactional
+    public void deleteUser(int id) {
+        entityManager.remove(getUser(id));
+    }
+
+    @Override
+    @Transactional
+    public User getUser(int id) {
+        return entityManager.find(User.class, id);
+    }
+}
